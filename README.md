dbf8bcfb6a3f0a56d7233b6d29d0a90ed0d2cde3
Zach Thomas <zach.thomas1.zt@gmail.com>
2020-11-01 22:31:15 -0600

    Updated for x220

--- README.md
+++ README.md
@@ -9,4 +9,11 @@ cd ~/.dotfiles
 
 `stow <file symlink>`
 
+##Notes for install
 For Arch-linux users: I had to un-install the grml-zsh-config package and reboot: yay -Rncs grml-zsh-config
+
+Install:
+- dwm
+- dwmblocks
+- st
+- run font script
--- scripts/dwmbar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true; do
-    MUSIC=$(`mpc current`)
-   TIME=`date +"%F %I:%M %p"`
-   VOLUME=$( amixer sget Master | grep -e 'Front Left:' | \
-       sed 's/[^\[]*\[\([0-9]\{1,3\}%\).*\(on\|off\).*/\2 \1/' | sed 's/off/M/' | sed 's/on //' )
-   xsetroot -name " $MUSIC | $VOLUME | $TIME "
-   sleep 1s    # Update time every minute
-done &
